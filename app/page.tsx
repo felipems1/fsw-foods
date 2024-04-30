f80875cac70caed5a8fe80f8b1dd7a19e984e4d4
felipe <083felype@gmail.com>
2024-04-29 22:27:48 -0300

    style: add Eslint rules

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div className="mx-6 bg-red-500 py-5">hello word</div>;
-};
+  return <div className="mx-6 bg-red-500 py-5">hello word</div>
+}
 
-export default Home;
+export default Home
