f3ace8f3ed7d1f9a38f024976d77af820b1566dd
felipe <083felype@gmail.com>
2024-04-30 01:05:16 -0300

    feat: add second promo banner to home page

---
+++ app/_components/promo-banner.tsx
@@ -0,0 +1,14 @@
+import Image, { ImageProps } from 'next/image'
+
+export function PromoBanner(props: ImageProps) {
+  return (
+    <Image
+      width={0}
+      height={0}
+      className="h-auto w-full object-contain"
+      sizes="100vw"
+      quality={100}
+      {...props}
+    />
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import { CategoryList } from './_components/category-list'
 import { Header } from './_components/header'
 import { Search } from './_components/search'
@@ -6,6 +5,7 @@ import { ProductList } from './_components/product-list'
 import { Button } from './_components/ui/button'
 import { ChevronRightIcon } from 'lucide-react'
 import { db } from './_lib/prisma'
+import { PromoBanner } from './_components/promo-banner'
 
 export default async function Home() {
   const products = await db.product.findMany({
@@ -36,14 +36,9 @@ export default async function Home() {
       </div>
 
       <div className="px-5 pt-6">
-        <Image
+        <PromoBanner
           src="/banner-mobile-01.png"
           alt="Até 30% de desconto em pizzas!"
-          width={0}
-          height={0}
-          className="h-auto w-full object-contain"
-          sizes="100vw"
-          quality={100}
         />
       </div>
 
@@ -58,6 +53,13 @@ export default async function Home() {
           </Button>
         </div>
         <ProductList products={products} />
+      </div>
+
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="/banner-mobile-02.png"
+          alt="A partir de R$17,90 em lanches"
+        />
       </div>
     </>
   )
