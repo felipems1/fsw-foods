6b0389a6aa3fcdbe885578cae488a3970ea4b24e
felipe <083felype@gmail.com>
2024-05-01 00:55:37 -0300

    feat: add cart button to product page

--- app/products/[id]/_components/product-info.tsx
+++ app/products/[id]/_components/product-info.tsx
@@ -141,6 +141,10 @@ export function ProductInfo({
         <h3 className="px-5 font-semibold">Sucos</h3>
         <ProductList products={complementaryProducts} />
       </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar à sacola</Button>
+      </div>
     </div>
   )
 }
