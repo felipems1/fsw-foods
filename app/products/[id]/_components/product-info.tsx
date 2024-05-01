ec3dc38096c02f0dd99548aa48e514b3a04ed8dc
felipe <083felype@gmail.com>
2024-05-01 00:51:20 -0300

    fix: show delivery time on product page

--- app/products/[id]/_components/product-info.tsx
+++ app/products/[id]/_components/product-info.tsx
@@ -125,13 +125,9 @@ export function ProductInfo({
               <TimerIcon size={14} />
             </div>
 
-            {Number(product.restaurant.deliveryFee) > 0 ? (
-              <p className="text-xs font-semibold">
-                {formatCurrency(Number(product.restaurant.deliveryFee))}
-              </p>
-            ) : (
-              <p className="text-xs font-semibold">Grátis</p>
-            )}
+            <p className="text-xs font-semibold">
+              {product.restaurant.deliveryTimeMinutes} min
+            </p>
           </div>
         </Card>
       </div>
