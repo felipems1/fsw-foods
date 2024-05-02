8ce7c9bc0d3229e28b96b26115b68044b07f97bc
felipe <083felype@gmail.com>
2024-05-01 22:29:40 -0300

    feat: add recommended products page

---
+++ app/products/recommended/page.tsx
@@ -0,0 +1,39 @@
+import { Header } from '@/app/_components/header'
+import { ProductItem } from '@/app/_components/product.item'
+import { db } from '@/app/_lib/prisma'
+
+export default async function RecommendedProducts() {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 20,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  })
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="mb-6 text-lg font-semibold">Pedidos Recomendados</h2>
+        <div className="grid grid-cols-2 gap-6">
+          {products.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              className="min-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
