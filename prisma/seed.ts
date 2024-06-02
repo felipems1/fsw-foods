1cb8a74192d4df168118ae3344aeec577477e6d1
felipe <083felype@gmail.com>
2024-06-01 23:41:50 -0300

    feat: add import in prisma client

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { PrismaClient } = require('@prisma/client')
+import { PrismaClient } from '@prisma/client'
 
 const prismaClient = new PrismaClient()
 
