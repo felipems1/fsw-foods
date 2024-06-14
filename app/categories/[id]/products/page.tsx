a2e77bf401564116500c600cab93589682d4a9ab
felipe <083felype@gmail.com>
2024-06-14 19:25:21 -0300

    feat: add screen desktop in categories page

--- app/categories/[id]/products/page.tsx
+++ app/categories/[id]/products/page.tsx
@@ -34,9 +34,9 @@ export default async function Categories({ params: { id } }: CategoriesProps) {
   return (
     <>
       <Header />
-      <div className="px-5 py-6">
+      <div className="px-5 py-6 lg:px-[128px]">
         <h2 className="mb-6 text-lg font-semibold">{category.name}</h2>
-        <div className="grid grid-cols-2 gap-6">
+        <div className="grid grid-cols-2 gap-6 lg:grid-cols-6">
           {category?.products.map((product) => (
             <ProductItem
               key={product.id}
