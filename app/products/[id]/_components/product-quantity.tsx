50a08f521bcb3c4af5934375ea05ccb595e996fa
felipe <083felype@gmail.com>
2024-06-14 20:48:15 -0300

    feat: add product quantity component

---
+++ app/products/[id]/_components/product-quantity.tsx
@@ -0,0 +1,96 @@
+import { DeliveryInfo } from '@/app/_components/delivery-info'
+import { DiscountBadge } from '@/app/_components/discount-badge'
+import { Button } from '@/app/_components/ui/button'
+import {
+  calculateProductTotalPrice,
+  formatCurrency,
+} from '@/app/_helpers/price'
+import { Prisma } from '@prisma/client'
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
+import Image from 'next/image'
+
+interface ProductQuantityProps {
+  product: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: true
+    }
+  }>
+  increaseQuantityClick: () => void
+  decreaseQuantityClick: () => void
+  quantity: number
+}
+
+export function ProductQuantity({
+  product,
+  decreaseQuantityClick,
+  increaseQuantityClick,
+  quantity,
+}: ProductQuantityProps) {
+  return (
+    <>
+      <div className="flex items-center gap-[0.375rem] px-5">
+        <div className="relative h-6 w-6">
+          <Image
+            src={product.restaurant.imageUrl}
+            alt={product.restaurant.name}
+            fill
+            sizes="100%"
+            className="rounded-full object-cover"
+          />
+        </div>
+        <span className="text-xs text-muted-foreground">
+          {product.restaurant.name}
+        </span>
+      </div>
+
+      <h1 className="mb-2 mt-1 px-5 text-xl font-semibold">{product.name}</h1>
+
+      <div className="flex items-center justify-between px-5">
+        <div>
+          <div className="flex items-center gap-2">
+            <h2 className="text-xl font-semibold">
+              {formatCurrency(calculateProductTotalPrice(product))}
+            </h2>
+            {product.discountPercentage > 0 && (
+              <DiscountBadge product={product} />
+            )}
+          </div>
+
+          {product.discountPercentage > 0 && (
+            <p className="text-sm text-muted-foreground">
+              De: {formatCurrency(Number(product.price))}
+            </p>
+          )}
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Button
+            size="icon"
+            variant="ghost"
+            className="border border-solid border-muted-foreground"
+            onClick={decreaseQuantityClick}
+          >
+            <ChevronLeftIcon />
+          </Button>
+          <span className="w-4 text-center">{quantity}</span>
+          <Button
+            size="icon"
+            className="border border-solid border-muted-foreground"
+            onClick={increaseQuantityClick}
+          >
+            <ChevronRightIcon />
+          </Button>
+        </div>
+      </div>
+
+      <div className="px-5">
+        <DeliveryInfo restaurant={product.restaurant} />
+      </div>
+
+      <div className="mt-6 space-y-3 px-5">
+        <h3 className="font-semibold">Sobre</h3>
+        <p className="text-sm text-muted-foreground">{product.description}</p>
+      </div>
+    </>
+  )
+}
